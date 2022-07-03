88153e9061fe50638e0de9cbd02524fc09ed667d
huangyan <huangydyn@foxmail.com>
2022-07-03 23:03:57 +0800

    init commit

---
+++ src/main/java/com/huangydyn/JavaPulsarExampleApplication.java
@@ -0,0 +1,12 @@
+package com.huangydyn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPulsarExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaPulsarExampleApplication.class, args);
+	}
+}
